9005b650aae284b6e3262b04f7ad3e6d488d1ccf
Adam Stockermans <adamstox@gmail.com>
2019-05-07 01:18:36 -0400

    dragging and dropping and snapping to nearest column

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,5 +1,4 @@
 import 'phaser';
-import logoImg from "../assets/logo.png";
 import B from "../assets/B.png";
 import BJ from "../assets/BJ.png";
 import BP from "../assets/BP.png";
@@ -9,14 +8,15 @@ import JBP from "../assets/JBP.png";
 import PBJ from "../assets/PBJ.png";
 import JBJ from "../assets/JBJ.png";
 import PBP from "../assets/PBP.png";
+import config from "../config/config.js";
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super('Game');
   }
 
+
   preload() {
-    this.load.image("logo", logoImg);
     this.load.image("B", B);
     this.load.image("BJ", BJ);
     this.load.image("BP", BP);
@@ -29,13 +29,55 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create() {
-    const B = this.add.image(400, 150, "B");
-    B.setInteractive();
-    this.input.on('gameobjectdown',this.onObjectClicked);
-  }
+    const plateCount = 4;
+    let plates = [];
+    for (let i = 0; i < plateCount; i++) {
+      const plate = {
+        x: ((i+1)*config.width)/(plateCount+1),
+        y: config.height - 200,
+        stack: [],
+      };
+      plates.push(plate);
+    }
+    let B = this.add.image(100, 100, 'B').setInteractive();
+    this.input.setDraggable(B);
 
-  onObjectClicked(pointer, gameObject) {
-    gameObject.x += 10;
-  };
+
+    this.input.on('dragstart', function (pointer, gameObject) {
+
+        this.children.bringToTop(gameObject);
+
+    }, this);
+
+    this.input.on('dragend', function (pointer, gameObject) {
+      let plate;
+        console.log(plates.length);
+      for(let i = 0; i < plates.length; i++) {
+        const dist = Phaser.Math.Distance.Between(plates[i].x, plates[i].y, gameObject.x, gameObject.y)
+        console.log(!plate);
+        if (!plate || dist < plate.dist) {
+          plate = {
+            x: plates[i].x,
+            y: plates[i].y,
+            dist: dist,
+          };
+        }
+      }
+      console.log(plate)
+      if (plate) {
+        gameObject.x = plate.x;
+        gameObject.y = plate.y;
+      };
+
+    }, this);
+    
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+
+        gameObject.x = dragX;
+        gameObject.y = dragY;
+
+    });
+
+  }
 
 }
